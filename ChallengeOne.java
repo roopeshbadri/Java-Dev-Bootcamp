5f5bc3a1db90b00fdf28335f568594a61e08f950
roopeshbadri <badriroopesh@gmail.com>
2025-01-09 11:53:42 +0530

    Challenge One finished

---
+++ ChallengeOne.java
@@ -0,0 +1,13 @@
+
+
+public class ChallengeOne {
+    public static void main(String[] args) {
+        System.out.println(" BBB   RRRR");
+        System.out.println("B   B  R  R");
+        System.out.println("B   B  R  R");
+        System.out.println("B B    RR");
+        System.out.println("B   B  R R");
+        System.out.println("B   B  R   R");
+        System.out.println(" BBB   R    R");
+    }
+}
